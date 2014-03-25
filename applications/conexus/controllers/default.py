083421c7227c5c4dec8f94b6bc21337dc275e147
Trevor Overman <troverman@gmail.com>
2014-03-25 16:31:50 -0400

    conexus

--- applications/conexus/controllers/default.py
+++ applications/conexus/controllers/default.py
@@ -1,92 +1,188 @@
 ################################################################
 ####controllers#################################################
 ################################################################
-
-################################
-####api#########################
-################################
-def api():
-
-    return dict(message=T('Hello World'))
-    
+  
 ################################
 ####about#######################
 ################################
 def about():
-
     return dict(message=T('Hello World'))
 
 ################################
 ####account#####################
 ################################
 def account():
-
     return dict(message=T('Hello World'))    
+
+################################
+####contact#####################
+################################
+def contact():
+    return dict(message=T('Hello World'))
+    
+################################
+####discover####################
+################################
+def discover():
+    return dict()
+
+################################
+####event#######################
+################################
+def event():
+    try:
+        event_from_url = db(db.event.url_title == request.args(0)).select()[0]
+    except IndexError:
+        redirect(URL('events'))
+
+    return dict(event_from_url=event_from_url)
+
+################################
+####event#######################
+################################
+def events():
+    event_list = db(db.event).select()
+    return dict(event_list=event_list)  
+  
+################################
+####faq#########################
+################################
+def faq():
+    return dict()
+
+################################
+####feed########################
+################################
+def feed():
+    return dict()
 
 ################################
 ####group#######################
 ################################
 def group():
-
     return dict(message=T('Hello World'))
 
 ################################
 ####groups######################
 ################################
 def groups():
-
     return dict(message=T('Hello World'))    
+
+################################
+####help########################
+################################
+def help():
+    return dict()
 
 ################################
 ####index#######################
 ################################
 def index():
+    project_list = db(db.project).select()
+    event_list = db(db.event).select()
 
-    return dict(message=T('Hello World'))
+    return dict(
+        project_list=project_list,
+        event_list=event_list,
+    )
 
 ################################
 ####member######################
 ################################
 def member():
+    try:
+        member_from_url = db(db.auth_user.username == request.args(0)).select()[0]
+    except IndexError:
+        redirect(URL('index'))
+
+    return dict(member_from_url=member_from_url)
+################################
+####mission#####################
+################################
+def mission():
+    return dict()
+
+################################
+####positions###################
+################################
+def positions():
+    return dict()
     
+################################
+####privacy#####################
+################################
+def privacy():
     return dict()
 
 ################################
 ####projects####################
 ################################    
 def projects():
-
-    return dict(message=T('Hello World'))
+    project_list = db(db.project).select()
+    create_project = SQLFORM(db.project)
+    return dict(
+        project_list=project_list,
+        create_project=create_project,
+    )
 
 ################################
 ####project#####################
 ################################
 def project():
+    try:
+        project_from_url = db(db.project.url_title == request.args(0)).select()[0]
+        project_member_array = db(db.project_member.project_id == project_from_url['id']).select()
 
+    except IndexError:
+        redirect(URL('projects'))
+
+    return dict(
+        project_from_url=project_from_url,
+        project_member_array=project_member_array,
+    )
+
+
+################################
+####search######################
+################################
+def search():
     return dict(message=T('Hello World'))
+
+################################
+####tasks#######################
+################################
+def tasks():
+    return dict()
+
+################################
+####terms#######################
+################################
+def terms():
+    return dict()
 
 ################################
 ####thread######################
 ################################
 def thread():
+    return dict(message=T('Hello World'))
 
+################################
+####stats#######################
+################################
+def stats():
     return dict(message=T('Hello World'))
 
 ################################
 ####threads#####################
 ################################
-def index():
-
+def thread():
     return dict(message=T('Hello World'))
 
 ################################
 ####transparency################
 ################################
 def transparency():
-
     return dict(message=T('Hello World'))    
-
-
 
 
 
@@ -94,41 +190,54 @@ def transparency():
 ####helpers#####################################################
 ################################################################
 
-
-
-
 @cache.action()
 def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
     return response.download(request, db)
 
-
+@auth.requires_login()
 def call():
-    """
-    exposes services. for example:
-    http://..../[app]/default/call/jsonrpc
-    decorate with @services.jsonrpc the functions to expose
-    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-    """
     return service()
-
 
 @auth.requires_signature()
 def data():
-    """
-    http://..../[app]/default/data/tables
-    http://..../[app]/default/data/create/[table]
-    http://..../[app]/default/data/read/[table]/[id]
-    http://..../[app]/default/data/update/[table]/[id]
-    http://..../[app]/default/data/delete/[table]/[id]
-    http://..../[app]/default/data/select/[table]
-    http://..../[app]/default/data/search/[table]
-    but URLs must be signed, i.e. linked with
-      A('table',_href=URL('data/tables',user_signature=True))
-    or with the signed load operator
-      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
-    """
     return dict(form=crud())
+
+@service.xmlrpc
+def test_add(a,b):
+    number_sum = a+b
+    db.testing.insert(number_sum=number_sum)
+    return a+b
+
+def test():
+    from xmlrpclib import ServerProxy
+    from google.appengine.api import urlfetch
+    URL = "https://troverman:trev77922@troverman-conexus.appspot.com/conexus/default/call/xmlrpc"
+    urlfetch.set_default_fetch_deadline(60)
+    service = ServerProxy(URL, verbose=True)
+    test = service.test_add(1, 2)
+    return test
+
+
+
+################################################################
+####ajax########################################################
+################################################################
+
+################################
+####ajax_join_event#############
+################################                     
+def ajax_join_event():
+    event_id = request.vars.itervalues()
+    for x in event_id:
+        event_id_trim = int(x)
+        
+    #LOGIC
+    #db.collection_member.insert(user_id = auth.user_id, collection_id = event_id_trim, event_usergroup_id=1)
+    #jquery = "jQuery('.flash').html('joined').slideDown().delay(1000).slideUp();"
+    #jquery += "$('#join-event-%s').fadeToggle(100);" % collection_id_trim
+    jquery = "jQuery('.flash').html('joined').slideDown().delay(1000).slideUp();alert('hello')"
+    return jquery   
+
+def echo():
+    return request.vars.name
+
--- applications/conexus/models/db.py
+++ applications/conexus/models/db.py
@@ -27,24 +27,15 @@ else:
 response.generic_patterns = ['*'] if request.is_local else []
 ## (optional) optimize handling of static files
 # response.optimize_css = 'concat,minify,inline'
-# response.optimize_js = 'concat,minify,inline'
-
-#########################################################################
-## Here is sample code if you need for
-## - email capabilities
-## - authentication (registration, login, logout, ... )
-## - authorization (role based authorization)
-## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
-## - old style crud actions
-## (more options discussed in gluon/tools.py)
-#########################################################################
 
 from gluon.tools import Auth, Crud, Service, PluginManager, prettydate
+request.vars._next = None
 auth = Auth(db)
+auth.next = None
 crud, service, plugins = Crud(db), Service(), PluginManager()
-
 ## create all tables needed by auth if not custom tables
-auth.define_tables(username=False, signature=False)
+auth.define_tables(username=True, signature=False)
+auth.settings.allow_basic_login = True
 
 ## configure email
 mail = auth.settings.mailer
@@ -57,27 +48,58 @@ auth.settings.registration_requires_verification = False
 auth.settings.registration_requires_approval = False
 auth.settings.reset_password_requires_verification = True
 
-## if you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
-## register with janrain.com, write your domain:api_key in private/janrain.key
-from gluon.contrib.login_methods.rpx_account import use_janrain
-use_janrain(auth, filename='private/janrain.key')
+################################################################
+####database_tables#############################################
+################################################################
 
-#########################################################################
-## Define your tables below (or better in another model file) for example
-##
-## >>> db.define_table('mytable',Field('myfield','string'))
-##
-## Fields can be 'string','text','password','integer','double','boolean'
-##       'date','time','datetime','blob','upload', 'reference TABLENAME'
-## There is an implicit 'id integer autoincrement' field
-## Consult manual for more options, validators, etc.
-##
-## More API examples for controllers:
-##
-## >>> db.mytable.insert(myfield='value')
-## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
-## >>> for row in rows: print row.id, row.myfield
-#########################################################################
+################################
+####member_location#############
+################################
+db.define_table('member_location',
+    Field('member_id','string'),
+    Field('time_stamp','datetime'),
+    Field('location_lat','string'),
+    Field('location_lng','string'),
+)
 
-## after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
+################################
+####testing#####################
+################################
+db.define_table('testing',
+    Field('number_sum','text'),
+)
+
+################################
+####project#####################
+################################
+db.define_table('project',
+    Field('url_title','string'),
+    Field('title','string'),
+)
+
+################################
+####project_member##############
+################################
+db.define_table('project_member',
+    Field('member_id','string'),
+    Field('project_id','string'),
+)
+
+################################
+####event#######################
+################################
+db.define_table('event',
+    Field('url_title','string'),
+    Field('title','string'),
+)
+
+################################
+####event_member################
+################################
+db.define_table('event_member',
+    Field('member_id','string'),
+    Field('event_id','string'),
+)
+
+
+
